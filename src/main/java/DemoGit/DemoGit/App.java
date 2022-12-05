88b622187959f0ffbdaeab598f267cb997bbebcf
chethanrnp <chethan.supernova@gmail.com>
2022-12-05 10:15:52 +0530

    Demo

---
+++ src/main/java/DemoGit/DemoGit/App.java
@@ -0,0 +1,13 @@
+package DemoGit.DemoGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
