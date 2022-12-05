1a4fdb5c0674107f4aa617d429d818db5a0565f2
chethanrnp <chethan.supernova@gmail.com>
2022-12-05 12:01:22 +0530

    Commit

---
+++ src/test/java/DemoGit/DemoGit/p3.java
@@ -0,0 +1,20 @@
+package DemoGit.DemoGit;
+
+public class p3 {
+
+	public static void main(String[] args) {
+		String s="madam";
+		String rev="";
+		
+		for (int i = s.length()-1; i>=0; i--) {
+			rev=rev+s.charAt(i);
+		}
+		
+		if(s.equals(rev))
+			System.out.println("Is polindrome");
+		
+		else
+			System.out.println("Is not a polindrome");
+	}
+
+}
